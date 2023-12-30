2ac307e2e9fa3ccda06b94c6990e7ebccb6ebf3b
mbz4b8 <149974307+mbz4b8@users.noreply.github.com>
2023-12-29 22:39:31 -0600

    final files

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key="8516f7cb0f1db997b3f200ed355d169f"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "afc0295228f5419dbde0a0a01612b6ff"
